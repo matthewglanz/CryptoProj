8f7867d671140a08ddb12588466069c5c8c0dc8f
Matthew G <matthewglanz@gmail.com>
2023-03-23 15:20:33 -0400

    Added basic ATM functionality to server

--- SSH_server.py
+++ SSH_server.py
@@ -3,6 +3,8 @@ import socket
 HOST = 'localhost'
 PORT = 1234
 
+balance = 1000
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
   sock.bind((HOST, PORT))
   sock.listen()
@@ -17,9 +19,37 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
       if not data:
         break
       print(f"Received: {data.decode('utf-8').strip()}")
-      message = input("Enter message: ")
-      conn.sendall(message.encode('utf-8'))
-      
+      #balance
+      if(data.decode('utf-8').strip() == "Balance"):
+        conn.sendall(f"Your current balance is {balance}".encode('utf-8'))
+      #withdraw
+      if(data.decode('utf-8').strip() == "Withdraw"):
+        #send message asking for amount
+        conn.sendall("How much would you like to withdraw?".encode('utf-8'))
+        #receive amount
+        data = conn.recv(1024)
+        amount = int(data.decode('utf-8').strip())
+        if(amount > balance):
+          conn.sendall("Insufficient Funds".encode('utf-8'))
+        else:
+          balance = balance - amount
+          conn.sendall(f"Your new balance is {balance}".encode('utf-8'))
+      #deposit
+      if(data.decode('utf-8').strip() == "Deposit"):
+        #send message asking for amount
+        conn.sendall("How much would you like to deposit?".encode('utf-8'))
+        #receive amount
+        data = conn.recv(1024)
+        amount = int(data.decode('utf-8').strip())
+        balance = balance + amount
+        conn.sendall(f"Your new balance is {balance}".encode('utf-8'))
+      #exit
+      if(data.decode('utf-8').strip() == "Exit"):
+        conn.sendall("Goodbye!".encode('utf-8'))
+        break
+    conn.close()
+
+          
     
   
   
